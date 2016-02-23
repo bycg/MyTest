40913bc8f983177ccd4f864f4e539decb2ca4745
qiye <qiye@lianjia.com>
2016-02-23 20:27:06 +0800

    11

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-       System.out.println("Hello,Amigo");
+       System.out.println("Hello111111,Amigo");
        
     }
 }
